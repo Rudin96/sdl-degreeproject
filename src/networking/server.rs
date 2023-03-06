5c9538bb20bf0fe962aa4ea8aa1c7ceb717f1555
Tobias Rudin <tobias_rudin@hotmail.se>
2023-03-06 17:23:39 +0100

    .

--- src/networking/server.rs
+++ src/networking/server.rs
@@ -14,23 +14,7 @@ pub fn createlan() {
 }
 
 fn create() -> std::io::Result<()> {
-    let socket = UdpSocket::bind(IP_ADDRESS)?;
-
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
-        let mut buf = [0; 10];
-        let (amt, src) = socket.recv_from(&mut buf)?;
-
-        for e in buf {
-            println!("Message line in buffer is: {e}");
-        }
-
-        // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-        let buf = &mut buf[..amt];
-        buf.reverse();
-        socket.send_to(buf, &src)?;
-
-    Ok(())
+    //TODO implement udp server host here.
 }
 
 pub fn createwan() {
